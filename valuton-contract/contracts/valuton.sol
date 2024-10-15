b147f605afcb25436dad4cd80b8dd7411f4298e9
harodggg <haroldtiansheng@gmail.com>
2024-10-15 17:30:22 +0800

    doc: add some desgin for valuton contract
    Signed-off-by: harodggg <haroldtiansheng@gmail.com>

--- valuton-contract/contracts/valuton.sol
+++ valuton-contract/contracts/valuton.sol
@@ -1,15 +1,39 @@
 contract Valuton {
 
+    bool PauseStatus = true;
+
+    map (address,timestrap) = [{"userAddress":"timestrap"}];  // unix time
+
+    map (address,amount)  = [{"userAddress":"amount"}];
 
     function withdrawLido() public {
 
+        if !PauseStatus {
+            revert
+        }
+
+    }
+
+    function dispatch() public {
+        if !PasuerStatus {
+            revert
+        }
     }
 
     function depositLido() public {
-
+        if !PauserStatus {
+            revert
+        }
     }
 
+
+
+
     function reportLido() public {
+        // 将合约的其他功能关闭
+        PauseStatus = false;
+
+        // 存入保证金
     }
 
 }
